949f9c52d380172fa5185eae2239c31a20e7f33e
= <=>
2021-09-24 22:12:09 +0200

    Primitive Types

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const interestRate = 0.3;
-interestRate = 1;
-console.log(interestRate);
+let name = 'Mosh'; // String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined;
+let selectedColor = null;
